42d2ade82da4597a5db8c16fa352613760bf4f66
Brain Atoms <badams6453@gmail.com>
2023-06-25 18:11:58 -0400

    required authorization to delete books

--- Develop/client/src/pages/SavedBooks.js
+++ Develop/client/src/pages/SavedBooks.js
@@ -20,26 +20,23 @@ const SavedBooks = () => {
 
   const userData = data?.me || {};
 
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
-    // get token
+  async function handleDeleteBook(bookId) {
+     
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
       return false;
     }
-
+    
     try {
-      const { data } = await removeBook({
-        variables: { bookId },
+      await removeBook({
+        variables: {bookId: bookId}
       });
-
-      // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
-  };
+  }
 
   if (loading) {
     return <h2>LOADING...</h2>;
